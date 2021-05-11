6b591dac9db53890c409d6daf0fb21dfc3181f83
martijn van de ree <mrtjnvanderee@gmail.com>
2021-05-11 13:33:13 +0200

    deploy

--- components/contactForm/onSubmit.tsx
+++ components/contactForm/onSubmit.tsx
@@ -1,4 +1,4 @@
-import { fetcher } from "../../functions/fetcher";
+
 
 type IValues = {
   name: string;
@@ -11,20 +11,8 @@ type IValues = {
 export const onSubmit = async (values: any, test: any) => {
   const { name, email, message, instantie, subject } = values;
 
-  const data = await fetcher(
-    [
-      "https://us-central1-website-dennis-stassen.cloudfunctions.net/app",
-      "mail",
-    ],
-    {
-      name,
-      email,
-      message,
-      instantie,
-      subject,
-    }
-  );
-  console.log(data, 'data')
+
+
 
   test.resetForm({});
 };
