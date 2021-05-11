38b7320856e801311c1ae5980028dc40097a89a6
martijn van de ree <mrtjnvanderee@gmail.com>
2021-05-11 13:25:02 +0200

    deploy

--- functions/fetcher.ts
+++ functions/fetcher.ts
@@ -1,14 +1,17 @@
 
 
 
+
+
+
 const addTwoStrings = (accumulator: string, currentValue: any) =>
   `${accumulator}/${currentValue}`;
 
-const reduceArgs = (params: any[]) => params.reduce(addTwoStrings);
+export const reduceArgs = (params: any[]) => params.reduce(addTwoStrings);
 
 
 
-const MakeCompleteURL = (url: string[]): string => {
+export const MakeCompleteURL = (url: string[]): string => {
   if (url.length === 1) return url[0];
   return reduceArgs(url);
 };
