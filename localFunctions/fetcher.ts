70b1e2a96ceb688bc025f86364dffe01479aa402
martijn van de ree <mrtjnvanderee@gmail.com>
2021-06-04 10:25:08 +0200

    added large media

--- localFunctions/fetcher.ts
+++ localFunctions/fetcher.ts
@@ -1,12 +1,7 @@
-
-
-
 const addTwoStrings = (accumulator: string, currentValue: any) =>
   `${accumulator}/${currentValue}`;
 
 export const reduceArgs = (params: any[]) => params.reduce(addTwoStrings);
-
-
 
 export const MakeCompleteURL = (url: string[]): string => {
   if (url.length === 1) return url[0];
