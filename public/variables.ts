a9c06eb9c4dc594b2002107222e3a0ab1e1b69df
martijn van de ree <mrtjnvanderee@gmail.com>
2021-06-11 13:53:49 +0200

    changed nav

--- public/variables.ts
+++ public/variables.ts
@@ -1,2 +1,2 @@
 export const navNames = ["Home", "History", "Nature", "Human", "Quirky", "Space", "Tech"]
-export const navLinks = ["/", "/history", "/nature", "/human", "/quirky", "/space", "/tech"]
+export const navLinks = ["/", "/", "/nature", "/human", "/quirky", "/space", "/tech"]
