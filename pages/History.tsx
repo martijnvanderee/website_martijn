fb40bf9fe1e89a8d31058bfe810540b7e3cbe243
martijn van de ree <mrtjnvanderee@gmail.com>
2021-06-18 12:27:16 +0200

    change history

--- pages/History.tsx
+++ pages/History.tsx
@@ -48,9 +48,9 @@ const History: FunctionComponent<HistoryProps> = ({ posts, randomPosts }) => {
               </div>
             </Link>
 
-            <div className=" md:hidden h-2 w-full bg-almostWhite"></div>
+            {/* <div className=" md:hidden h-2 w-full bg-almostWhite"></div> */}
 
-            <div className="mb-10 md:mb-0">
+            {/* <div className="mb-10 md:mb-0">
               <div className="md:h-96 md:overflow-auto">
 
                 <div className="sticky md:static top-72 z-10 border-b-2 border-yellow p-3 md:mx-4 md:pt-0 bg-white">
@@ -66,22 +66,17 @@ const History: FunctionComponent<HistoryProps> = ({ posts, randomPosts }) => {
               <Link href={`/net-binnen/1`}>
                 <div className="text-2xl md:text-2xl font-semibold leading-tight text-grey ml-4 cursor-pointer" >Meer net binnen <span className="text-2xl md:text-xl font-semibold leading-tight text-purple">{">"}</span>  </div>
               </Link>
-            </div>
-
-
+            </div> */}
           </div>
 
-          <div className="hidden md:block bg-almostWhite h-0.5 w-full mt-8"></div>
+          {/* <div className="hidden md:block bg-almostWhite h-0.5 w-full mt-8"></div> */}
 
-          <div className="text-2xl mt-10 ml-4 font-bold text-grey hidden md:block">
+          {/* <div className="text-2xl mt-10 ml-4 font-bold text-grey hidden md:block">
             <h2 className="inline border-b border-almostWhite ">Lees ook één van onze andere verhalen</h2>
-          </div>
+          </div> */}
 
-          <div className="hidden md:block md:grid  md:grid-cols-3 pb-10 mt-2">
-
-
+          {/* <div className="hidden md:block md:grid  md:grid-cols-3 pb-10 mt-2">
             {randomPosts.posts.map((randomPost: PostData, index) => {
-
               const url = randomPost.slug
               return (
                 <Link href={`/${url}`} as={`/${url}`}>
@@ -105,11 +100,10 @@ const History: FunctionComponent<HistoryProps> = ({ posts, randomPosts }) => {
                 </Link>
               )
             })}
+          </div> */}
 
 
 
-
-          </div>
         </div>
       </main>
     </Layout>
