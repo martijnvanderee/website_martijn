613f0ddc402637118c6d4542d931fc8f24218445
martijn van de ree <mrtjnvanderee@gmail.com>
2021-06-18 15:43:19 +0200

    test

--- public/variables.ts
+++ public/variables.ts
@@ -1,4 +1,4 @@
-export const navNames = ["Home", "Nature", "Human", "Quirky", "Space", "Tech"]
-export const navLinks = ["/", "/nature", "/human", "/quirky", "/space", "/tech"]
+export const navNames = ["Home", "History", "Nature", "Human", "Quirky", "Space", "Tech"]
+export const navLinks = ["/", "/history", "/nature", "/human", "/quirky", "/space", "/tech"]
 export const numberOfPostsOnPage = 6
 export const amountOfPostFrontPage = 10
