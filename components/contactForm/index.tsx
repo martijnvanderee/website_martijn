5bb0f5b5a042bfce9f9e6b7fcf1aedbe3396a039
martijn van de ree <mrtjnvanderee@gmail.com>
2021-09-24 12:38:30 +0200

    added form

--- components/contactForm/index.tsx
+++ components/contactForm/index.tsx
@@ -4,29 +4,30 @@ export const ContactForm = () => {
 
     <div className="relative bg-white z-10 font-mono shadow-lg max-w-2xl mx-auto p-8 rounded-xl">
       <h2 className="my-8 font-mono text-2xl md:text-3xl font-bold text-black"> <span className="text-green">Waarmee</span> kan ik je helpen?</h2>
-      <form >
+      <form name="contact" action="/success" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact" />
         <div className="mb-6">
-          <input className="shadow-sm appearance-none border rounded-lg w-full py-3 px-4 text-gray-700 leading-tight text-xl  focus:outline-none focus:shadow-outline" id="naam" type="text" placeholder="Naam" />
+          <input className="shadow-sm appearance-none border rounded-lg w-full py-3 px-4 text-gray-700 leading-tight text-xl  focus:outline-none focus:shadow-outline" id="naam" type="text" placeholder="Naam" name="naam" />
         </div>
 
         <div className="mb-6">
-          <input className="shadow-sm appearance-none border rounded-lg w-full py-3 px-4 text-gray-700 leading-tight text-xl focus:outline-none focus:shadow-outline" id="naam" type="text" placeholder="Achternaam" />
+          <input className="shadow-sm appearance-none border rounded-lg w-full py-3 px-4 text-gray-700 leading-tight text-xl focus:outline-none focus:shadow-outline" id="naam" type="text" placeholder="Achternaam" name="achternaam" />
         </div>
 
         <div className="mb-6">
-          <input className="shadow-sm appearance-none border rounded-lg w-full py-3 px-4 text-gray-700 leading-tight text-xl  focus:outline-none focus:shadow-outline" id="naam" type="text" placeholder="Telefoonnummer" />
+          <input className="shadow-sm appearance-none border rounded-lg w-full py-3 px-4 text-gray-700 leading-tight text-xl  focus:outline-none focus:shadow-outline" id="naam" type="text" placeholder="Telefoonnummer" name="Telefoonnummer" />
         </div>
 
         <div className="mb-6">
-          <input className="shadow-sm appearance-none border rounded-lg w-full py-3 px-4 text-gray-700 leading-tight text-xl focus:outline-none focus:shadow-outline" id="naam" type="text" placeholder="E-mailadres" />
+          <input className="shadow-sm appearance-none border rounded-lg w-full py-3 px-4 text-gray-700 leading-tight text-xl focus:outline-none focus:shadow-outline" id="naam" type="text" placeholder="E-mailadres" name="email" />
         </div>
 
         <div className="mb-6">
-          <textarea className="w-full px-3 py-2 leading-tight text-gray-700 border rounded-lg focus:outline-none focus:shadow-outline text-xl" rows={6} placeholder="Waarmee kan ik je helpen?"></textarea>
+          <textarea className="w-full px-3 py-2 leading-tight text-gray-700 border rounded-lg focus:outline-none focus:shadow-outline text-xl" rows={6} placeholder="Waarmee kan ik je helpen?" name="vraag"></textarea>
         </div>
 
         <div className="flex justify-end  mb-6">
-          <button className="px-4 py-2 mb-6 border-2 border-green text-xl text-green rounded shadow-headerButton hover:shadow-headerButtonHover transition ease-in">Verstuur</button>
+          <button className="px-4 py-2 mb-6 border-2 border-green text-xl text-green rounded shadow-headerButton hover:shadow-headerButtonHover transition ease-in" type="submit">Verstuur</button>
         </div>
 
       </form>
